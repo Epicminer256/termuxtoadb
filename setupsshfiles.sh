4aa1ea9e11e571b70a542bc764f9a4ff07ef11a9
IanKing <72170170+Epicminer256@users.noreply.github.com>
2021-05-30 19:40:06 -0500

    Update setupsshfiles.sh

--- setupsshfiles.sh
+++ setupsshfiles.sh
@@ -1,4 +1,4 @@
-echo -------------------------------------
+echo _________________________________
 echo make sure you have your phone plugged thoughout this setup
 echo
 echo Have openssh installed on your phone
@@ -6,25 +6,25 @@ echo also run sshd -p8022 on termux
 echo
 echo Make sure your phone has a password by typing passwd
 echo Hit Ctrl+C to stop or press any key to continue
-echo -------------------------------------
+echo _________________________________
 echo 
 echo
 echo Hit ENTER or RETURN to continue
 read
 
 echo
-echo ------------------------------
+echo _________________________________
 echo Making local ssh launch scripts
-echo ------------------------------
+echo _________________________________
 echo "adb forward tcp:8022 tcp:8022 && ssh 127.0.0.1 -p8022 " >> PhoneSSH.sh
 chmod +x PhoneSSH.sh
 echo
 
-echo -------------------------------------
+echo _________________________________
 echo generating a ssh key for automatic login
 echo Just use the default directory for this key
 echo Also have a password NOT set so it is a automatic login
-echo -------------------------------------
+echo _________________________________
 ssh-keygen -t rsa -b 4096
 
 echo transfering the key to phone
@@ -33,10 +33,10 @@ ssh-copy-id 127.0.0.1 -p8022
 
 echo
 echo
-echo -------------------------------------
+echo _________________________________
 echo Now you are set up, you now have it setup to connect to your phone at any time
 echo Now you will be brought to your termux if this worked
-echo -------------------------------------
+echo _________________________________
 echo
 echo Connecting to the phone...
 ssh 127.0.0.1 -p8022
